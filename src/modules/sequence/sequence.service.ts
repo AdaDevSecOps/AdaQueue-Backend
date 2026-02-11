82f68099b02ef3c81e1bab9d51ec0d6660698c1d
Wuttichai <CWuttichai@ada-soft.com>
2026-02-11 14:11:28 +0700

    [fix] ปรับการรีเซ็ตค่าคิว และการดึงคิวจาก sql server

--- src/modules/sequence/sequence.service.ts
+++ src/modules/sequence/sequence.service.ts
@@ -42,6 +42,22 @@ export class SequenceService {
     const rows = await this.dataSource.query(sql);
     const v = rows?.[0]?.nextVal ?? rows?.[0];
     const num = typeof v === 'number' ? v : parseInt(String(v || '0'), 10);
+    
+    try {
+        const markSql = `
+            IF NOT EXISTS (SELECT 1 FROM sys.extended_properties WHERE name=N'ADA_SEQ_STARTED' AND major_id = OBJECT_ID(N'dbo.[${qn}]') AND value = N'1')
+            BEGIN
+                IF EXISTS (SELECT 1 FROM sys.extended_properties WHERE name=N'ADA_SEQ_STARTED' AND major_id = OBJECT_ID(N'dbo.[${qn}]'))
+                    EXEC sys.sp_updateextendedproperty @name=N'ADA_SEQ_STARTED', @value=N'1', @level0type=N'SCHEMA', @level0name=N'dbo', @level1type=N'SEQUENCE', @level1name=N'${name}';
+                ELSE
+                    EXEC sys.sp_addextendedproperty @name=N'ADA_SEQ_STARTED', @value=N'1', @level0type=N'SCHEMA', @level0name=N'dbo', @level1type=N'SEQUENCE', @level1name=N'${name}';
+            END
+        `;
+        await this.dataSource.query(markSql);
+    } catch (e) {
+        // ignore error
+    }
+
     return num;
   }
 
@@ -54,7 +70,8 @@ export class SequenceService {
         s.increment AS inc,
         s.maximum_value AS maxValue,
         s.is_cycling AS isCycle,
-        s.current_value AS currentValue
+        s.current_value AS currentValue,
+        (SELECT CAST(value AS nvarchar(10)) FROM sys.extended_properties WHERE name = 'ADA_SEQ_STARTED' AND major_id = s.object_id) as startedFlag
       FROM sys.sequences s
       WHERE s.name = '${qn}' AND s.schema_id = SCHEMA_ID('dbo')
     `;
@@ -65,6 +82,13 @@ export class SequenceService {
     const inc = typeof r.inc === 'number' ? r.inc : parseInt(String(r.inc || '1'), 10);
     const max = typeof r.maxValue === 'number' ? r.maxValue : parseInt(String(r.maxValue || '999'), 10);
     const cyc = !!r.isCycle;
+    const isStarted = r.startedFlag === '1';
+
+    // If not started yet, next value is the start value (usually 1)
+    if (!isStarted) {
+        return { next: r.startValue || 1 };
+    }
+
     let next = curr ? curr + inc : r.startValue || 1;
     if (next > max) next = cyc ? r.startValue || 1 : max;
     return { next };
@@ -87,11 +111,18 @@ export class SequenceService {
       const last = r?.[0]?.v as string | undefined;
       if (last !== ymd) {
         await this.dataSource.query(`ALTER SEQUENCE dbo.[${qn}] RESTART WITH 1`);
+        
+        // Update YMD and Reset STARTED flag to 0
         const upd = `
-IF EXISTS (SELECT 1 FROM sys.extended_properties WHERE name=N'ADA_LAST_RESET_YMD' AND major_id = OBJECT_ID(N'dbo.[${qn}]'))
-  EXEC sys.sp_updateextendedproperty @name=N'ADA_LAST_RESET_YMD', @value=N'${ymd}', @level0type=N'SCHEMA', @level0name=N'dbo', @level1type=N'SEQUENCE', @level1name=N'${name}';
-ELSE
-  EXEC sys.sp_addextendedproperty @name=N'ADA_LAST_RESET_YMD', @value=N'${ymd}', @level0type=N'SCHEMA', @level0name=N'dbo', @level1type=N'SEQUENCE', @level1name=N'${name}';
+            IF EXISTS (SELECT 1 FROM sys.extended_properties WHERE name=N'ADA_LAST_RESET_YMD' AND major_id = OBJECT_ID(N'dbo.[${qn}]'))
+                EXEC sys.sp_updateextendedproperty @name=N'ADA_LAST_RESET_YMD', @value=N'${ymd}', @level0type=N'SCHEMA', @level0name=N'dbo', @level1type=N'SEQUENCE', @level1name=N'${name}';
+            ELSE
+                EXEC sys.sp_addextendedproperty @name=N'ADA_LAST_RESET_YMD', @value=N'${ymd}', @level0type=N'SCHEMA', @level0name=N'dbo', @level1type=N'SEQUENCE', @level1name=N'${name}';
+
+            IF EXISTS (SELECT 1 FROM sys.extended_properties WHERE name=N'ADA_SEQ_STARTED' AND major_id = OBJECT_ID(N'dbo.[${qn}]'))
+                EXEC sys.sp_updateextendedproperty @name=N'ADA_SEQ_STARTED', @value=N'0', @level0type=N'SCHEMA', @level0name=N'dbo', @level1type=N'SEQUENCE', @level1name=N'${name}';
+            ELSE
+                EXEC sys.sp_addextendedproperty @name=N'ADA_SEQ_STARTED', @value=N'0', @level0type=N'SCHEMA', @level0name=N'dbo', @level1type=N'SEQUENCE', @level1name=N'${name}';
         `;
         await this.dataSource.query(upd);
       }
